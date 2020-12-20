a1fe3a6770e4851001880a9c4124646def3f0aee
Jordan <jlewis2008@live.com>
2020-12-20 14:47:09 -0500

    after while

---
+++ ifstatements.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    if(3 > 10){
+      echo " 3 is less than 10";
+    } elseif( 4 < 5 ) {
+      echo "of course 4 is less than 5";
+    } else {
+      echo "3 is not more than 10";
+    }
+  ?>
+</body>
+</html>
---
+++ switch.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+  $number = 11;
+
+  switch($number){
+    case 34:
+      echo "it is 34";
+      break;
+    case 37:
+      echo "it is 37";
+      break;
+    case 28:
+      echo "it is 28";
+      break;
+    case 10:
+      echo "it is 10";
+      break;
+    default:
+      echo "we could not find anything";
+      break;
+  }
+  ?>
+</body>
+</html>
